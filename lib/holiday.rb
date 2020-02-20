3db4320b7673dfdfeb2bd2233558248d58325099
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-20 04:06:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -108,13 +108,6 @@ all_winter_holiday_supplies(holiday_supplies)
 
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
   holiday_hash.each do | season, holiday|
      puts "#{season.capitalize}:"
      holiday.each do |holiday, item|
@@ -123,21 +116,14 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-all_supplies_in_holidays(holiday_supplies)
-
-
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
     end 
   end.flatten.compact
 end
-
-all_holidays_with_bbq(holiday_supplies)
 
 
 
